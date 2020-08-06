405d15d6e7adbfdf3faab70d0719f6cb315343a5
kingkoor <kingkoor@yahoo.com>
2020-08-06 10:56:50 -0400

    Shop page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-import HatPage from './components/hats.component';
+
 
 
 function App() {
@@ -12,7 +12,6 @@ function App() {
     {/* <HomePage/> */}
     <Switch>
       <Route exact={true} path='/' component={HomePage} />
-      <Route exact={true} path='/hats' component={HatPage} />
       <Route path='/shop' component={ShopPage} />
     </Switch>
 
